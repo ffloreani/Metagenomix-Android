bc8b18dcf616ba8ad93b5c80a066232460974ac2
matej95 <matej.saravanja@gmail.com>
2017-02-28 19:13:06 +0100

    BBMapWrapper update; rescaleScore methods finished

--- app/src/main/java/com/metagenomix/android/processing/BbMapWrapper.java
+++ app/src/main/java/com/metagenomix/android/processing/BbMapWrapper.java
@@ -1,6 +1,7 @@
 package com.metagenomix.android.processing;
 
 import android.content.Context;
+import android.util.FloatProperty;
 import android.util.Pair;
 
 import java.io.BufferedReader;
@@ -58,5 +59,68 @@ public class BbMapWrapper {
             e.printStackTrace();
         }
         return finalMapQScores;
+    }
+
+    public double findEntryScore(List<String> ln, double pScoreCutOff) {
+        int mxBitSc = 700;
+        int sigma2 = 3;
+        float mapq = Float.parseFloat(ln.get(4));
+        float mapq2 = mapq / (-10.0f);
+        double pscore = 1.0 - Math.pow(10.0, (double) mapq2);
+        return pscore;
+    }
+
+    public boolean findEntryScoreSkipFlag(double pScore, double pScoreCutOff) {
+        if(pScore < pScoreCutOff)
+            return true;
+        return false;
+    }
+
+    public Map<Integer, List<Double>> rescaleSamScoreU(Map<Integer, List<Double>> U, double maxScore, double minScore) {
+        double scalingFactor;
+        if (minScore < 0) {
+            scalingFactor = 100.0 / (maxScore - minScore);
+        } else {
+            scalingFactor = 1.0 / maxScore;
+        }
+
+        for(int rIdx: U.keySet()) {
+            if(minScore < 0) {
+                double value = U.get(rIdx).get(1) - minScore;
+                List<Double> keyList = U.get(rIdx);
+                keyList.add(1, value);
+                U.put(rIdx, keyList);
+            }
+            double val = Math.exp(U.get(rIdx).get(1) * scalingFactor);
+            List<Double> list = U.get(rIdx);
+            list.set(1, val);
+            list.set(3, val);
+            U.put(rIdx, list);
+        }
+        return U;
+    }
+
+    public Map<Integer, List<Double>> rescaleSamScoreNU(Map<Integer, List<List<Double>>> NU, double maxScore, double minScore) {
+        double scalingFactor;
+        if (minScore < 0) {
+            scalingFactor = 100.0 / (maxScore - minScore);
+        } else {
+            scalingFactor = 1.0 / maxScore;
+        }
+
+        for(int rIdx: NU.keySet()) {
+
+            NU.get(rIdx).get(3).set(0, 0.0);
+            for(int i=0; i< NU.get(rIdx).get(1).size(); i++) {
+                if(minScore < 0) {
+                    NU.get(rIdx).get(1).set(i, NU.get(rIdx).get(1).get(i) - minScore);
+                }
+                NU.get(rIdx).get(1).set(i, Math.exp(NU.get(rIdx).get(1).get(i) * scalingFactor));
+                if(NU.get(rIdx).get(1).get(i) > NU.get(rIdx).get(3).get(0)) {
+                    NU.get(rIdx).get(3).set(0, NU.get(rIdx).get(1).get(i));
+                }
+            }
+        }
+        return NU;
     }
 }
